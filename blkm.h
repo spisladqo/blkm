46a959803a76f5cf5c81bb6441a240112c29b139
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-07-28 11:20:01 +0300

    fix: remove extra function prototype

--- blkm.h
+++ blkm.h
@@ -6,5 +6,4 @@ struct skiplist;
 struct skiplist *skiplist_init(void);
 struct skiplist_node *skiplist_find_node(sector_t key, struct skiplist *sl);
 int skiplist_add(sector_t key, sector_t data, struct skiplist *sl);
-int skiplist_del(sector_t key, struct skiplist *sl);
 void skiplist_free(struct skiplist *sl);
